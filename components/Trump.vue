f6a7e053107315832456ab1e75e6d05e16d8c87e
ryo-1123 <union.adiinoyr_18@softbank.ne.jp>
2019-05-28 05:01:07 +0900

    Trump.vue cardInformationAddition

--- components/Trump.vue
+++ components/Trump.vue
@@ -1,21 +1,35 @@
 <template>
   <div class="trump-wrapper">
     <div v-for="n in 52" :key="n">
-      <img :src="trumpUrl[n-1]" width="100">
+      <img :src="trumpUrl[n-1].url" width="100">
     </div>
   </div>
 </template>
 
 <script>
-// トランプ用配列の準備
+// トランプの連想配列の作成
 const trumpUrl = Array(52)
 
 // 最大
 const max = trumpUrl.length
 
+// カードのnumber情報を持たせるための変数
+let num = 0
 // 配列に要素を入れる
 for (let i = 0; i < max; i++) {
-  trumpUrl[i] = require(`~/assets/images/trump/${i + 1}.gif`)
+  num++
+  // numが13の間の処理
+  if (num < 14) {
+    trumpUrl[i] = { 'url': require(`~/assets/images/trump/${i + 1}.gif`),
+      // カードnum情報
+      'num': num,
+      'on': 0
+    }
+    // numが13になったら1からスタート
+    if (num === 13) {
+      num = 0
+    }
+  }
 }
 
 // for文でurlの作成
